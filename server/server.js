c9aa911ae194fb0e849e594efeb95f661adbd1b9
Maysara <maysaracs1001@gmail.com>
2025-11-14 20:02:28 +0300

    aiController updated

--- server/server.js
+++ server/server.js
@@ -7,9 +7,11 @@ import express from 'express';
 import cors from 'cors';
 import "dotenv/config";
 import aiRouter from "./routes/aiRoutes.js";
+import connectCloudinary from "./configs/cloudinary.js";
 
 
 const app = express();
+await connectCloudinary();
 app.use(cors())
 app.use(express.json())
 app.use(clerkMiddleware())
